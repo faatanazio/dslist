7328bcc1a15ebc060cbb101e0e9bfd13854661b0
faatanazio <atanazio.fabricio@gmail.com>
2025-08-19 21:01:36 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -9,135 +9,122 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-//ctrl+shift+o importa o @
 @Entity
 @Table(name = "tb_game")
 public class Game {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	private Double score;
 	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
-	private Double score;
+	private String platforms;
+	
+	@Column(name = "img_url")
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-
-	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+    public Game() {
+    }
+    
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+		this.longDescription = longDescription;		
 	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getTitle() {
 		return title;
 	}
 
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 
 	public Integer getYear() {
 		return year;
 	}
 
-
 	public void setYear(Integer year) {
 		this.year = year;
 	}
-
 
 	public String getGenre() {
 		return genre;
 	}
 
-
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
 
-
 	public String getPlatforms() {
-		return platform;
+		return platforms;
 	}
 
-
-	public void setPlatforms(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
-
 
 	public Double getScore() {
 		return score;
 	}
 
-
 	public void setScore(Double score) {
 		this.score = score;
 	}
-
 
 	public String getImgUrl() {
 		return imgUrl;
 	}
 
-
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-
 
 	public String getShortDescription() {
 		return shortDescription;
 	}
 
-
 	public void setShortDescription(String shortDescription) {
 		this.shortDescription = shortDescription;
 	}
-
 
 	public String getLongDescription() {
 		return longDescription;
 	}
 
-
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
 	}
 
-// comparador de games
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -150,5 +137,4 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 }
